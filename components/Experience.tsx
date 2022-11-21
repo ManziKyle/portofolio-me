98b2b5180dc12d76e09b9690ebce245a1e232e78
ManziKyle <manzikyle@gmail.com>
2022-11-21 17:28:30 +0200

    'sec_commit'

---
+++ components/Experience.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Experience({}: Props) {
+  return (
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <h3 className='tittle'>Experience</h3>
+    </div>
+  )
+}
